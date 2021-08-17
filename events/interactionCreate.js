6991faa65edb2869d0276e5d6e7bba32d2104ec6
Java <elijahfajimi1@gmail.com>
2021-08-16 19:09:44 -0500

    Update handler
    Updated handler + preperation for context menus.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,16 +1,12 @@
 const client = require("../index");
-
 client.on("interactionCreate", async (interaction) => {
     // Slash Command Handling
     if (interaction.isCommand()) {
         await interaction.deferReply({ ephemeral: false }).catch(() => {});
-
         const cmd = client.slashCommands.get(interaction.commandName);
         if (!cmd)
             return interaction.followUp({ content: "An error has occured " });
-
         const args = [];
-
         for (let option of interaction.options.data) {
             if (option.type === "SUB_COMMAND") {
                 if (option.name) args.push(option.name);
@@ -22,5 +18,12 @@ client.on("interactionCreate", async (interaction) => {
         interaction.member = interaction.guild.members.cache.get(interaction.user.id);
 
         cmd.run(client, interaction, args);
+    }
+
+    // Context Menu Handling
+    if (interaction.isContextMenu()) {
+        await interaction.deferReply({ ephemeral: false });
+        const command = client.slashCommands.get(interaction.commandName);
+        if (command) command.run(client, interaction);
     }
 });
--- handler/index.js
+++ handler/index.js
@@ -1,9 +1,7 @@
 const { glob } = require("glob");
 const { promisify } = require("util");
 const { Client } = require("discord.js");
-
 const globPromise = promisify(glob);
-
 /**
  * @param {Client} client
  */
@@ -14,33 +12,60 @@ module.exports = async (client) => {
         const file = require(value);
         const splitted = value.split("/");
         const directory = splitted[splitted.length - 2];
-
         if (file.name) {
             const properties = { directory, ...file };
             client.commands.set(file.name, properties);
-            file.aliases?.forEach((alias) => {
-                client.aliases.set(alias, file.name)
-            })
         }
     });
-
     // Events
     const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
     eventFiles.map((value) => require(value));
-
     // Slash Commands
     const slashCommands = await globPromise(
         `${process.cwd()}/SlashCommands/*/*.js`
     );
-
+    const arrayOfSlashCommands = [];
+    slashCommands.map((value) => {
+        const file = require(value);
+const { glob } = require("glob");
+const { promisify } = require("util");
+const { Client } = require("discord.js");
+const globPromise = promisify(glob);
+/**
+ * @param {Client} client
+ */
+module.exports = async (client) => {
+    // Commands
+    const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
+    commandFiles.map((value) => {
+        const file = require(value);
+        const splitted = value.split("/");
+        const directory = splitted[splitted.length - 2];
+        if (file.name) {
+            const properties = { directory, ...file };
+            client.commands.set(file.name, properties);
+        }
+    });
+    // Events
+    const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
+    eventFiles.map((value) => require(value));
+    // Slash Commands
+    const slashCommands = await globPromise(
+        `${process.cwd()}/SlashCommands/*/*.js`
+    );
     const arrayOfSlashCommands = [];
     slashCommands.map((value) => {
         const file = require(value);
         if (!file?.name) return;
         client.slashCommands.set(file.name, file);
+
+        if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
         arrayOfSlashCommands.push(file);
     });
     client.on("ready", async () => {
+        await client.application.commands.set(arrayOfSlashCommands);
+    });
+};
         await client.application.commands.set(arrayOfSlashCommands);
     });
 };
